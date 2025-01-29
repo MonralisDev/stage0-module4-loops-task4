c9b239efc6dffeafd9b29b0da693b840d92215be
M <M>
2025-01-30 00:11:18 +0200

    ++

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        if (numberToGoUntil < toBreakWith) {
+            System.out.println("iterating till the end");
+            for (int i = 1; i <= numberToGoUntil; i++) {
+                System.out.println(i);
+            }
+        } else if (numberToGoUntil >= toBreakWith) { // Виправлена умова
+            for (int i = 1; i <= toBreakWith; i++) {
+                System.out.println(i);
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        NumberToBreakOn numberToBreakOn = new NumberToBreakOn();
+        numberToBreakOn.printNumbersUntilWithBreakOn(5, 5);
     }
 }
